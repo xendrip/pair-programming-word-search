a549d8b5c7420a1c919e0af88b08df21e87d6055
Rubin <rjhand@gmail.com>
2020-06-04 03:21:16 +0000

    Pair Programming: final 1.1

--- wordsearch.js
+++ wordsearch.js
@@ -12,21 +12,22 @@ const wordSearch = (letters, word) => {
       if (typeof arr[i] !== 'string') {
         return false;
       }
-      result += arr[i]; 
+      result += arr[i];
+      
       if (result.includes(word)) {
         return true;
       }
     }
- };
+  }
 
-const horizontalJoin = letters.map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-for (l of horizontalJoin) {
-  if (l.includes(word)) {
-    return true;
-  } 
-} 
-return false;
-}; 
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = wordSearch;
